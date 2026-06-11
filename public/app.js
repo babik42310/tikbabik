b3051f3bfc98c6d12ff3b69d39f0d7122676bb52
babik42310 <rgouvet19@gmail.com>
2026-06-11 10:26:14 +0200

    Lock premium mini games for free users

--- public/app.js
+++ public/app.js
@@ -5533,4 +5533,21 @@ function isProUser() {
     );
 }
 
+document.querySelectorAll("[data-pro='true']").forEach(element => {
+
+    element.addEventListener("click", event => {
+
+        if (isProUser()) {
+            return;
+        }
+
+        event.preventDefault();
+        event.stopImmediatePropagation();
+
+        goToProCheckout();
+
+    }, true);
+
+});
+
 console.log("FIN APP JS");
