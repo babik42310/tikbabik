62d0c575591dea99761b67b3e9e611f25ef23b4d
babik42310 <rgouvet19@gmail.com>
2026-06-17 15:38:54 +0200

    Limit PostgreSQL connections

--- server.js
+++ server.js
@@ -27,7 +27,10 @@ const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: {
         rejectUnauthorized: false
-    }
+    },
+    max: 3,
+    idleTimeoutMillis: 30000,
+    connectionTimeoutMillis: 10000
 });
 
 async function initDatabase() {
